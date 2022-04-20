c2f0e9a9369be0f7009560cfa9b4d70d4a0424ee
José Daniel Yapura <danielyapura@gmail.com>
2022-04-20 16:13:20 -0300

    otors cambios

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,8 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
+
+      // otros ambios
       _counter++;
     });
   }
@@ -69,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    
+
     //adsjflaksdjfa
     return Scaffold(
       appBar: AppBar(
